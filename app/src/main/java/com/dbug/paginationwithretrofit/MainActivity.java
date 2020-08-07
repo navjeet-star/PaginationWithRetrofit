944cb88daa9e96512dec2b5d1c7b0765e475c8a8
navjeet-star <knavjeet540@gmail.com>
2020-08-07 12:08:38 +0530

    Initial commit

---
+++ app/src/main/java/com/dbug/paginationwithretrofit/MainActivity.java
@@ -0,0 +1,174 @@
+package com.dbug.paginationwithretrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import com.dbug.paginationwithretrofit.api.MovieApi;
+import com.dbug.paginationwithretrofit.api.MovieService;
+import com.dbug.paginationwithretrofit.models.Result;
+import com.dbug.paginationwithretrofit.models.TopRatedMovies;
+
+import com.dbug.paginationwithretrofit.utils.PaginationScrollListener;
+import com.facebook.shimmer.ShimmerFrameLayout;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rv;
+    ProgressBar progressBar;
+    PaginationAdapter adapter;
+    LinearLayoutManager linearLayoutManager;
+    MovieService movieService;
+
+
+    private static final int PAGE_START = 1;
+    private boolean isLoading = false;
+    private boolean isLastPage = false;
+    // limiting to 5 for this tutorial, since total pages in actual API is very large. Feel free to modify.
+    private int TOTAL_PAGES = 5;
+    private int currentPage = PAGE_START;
+
+    private ShimmerFrameLayout mShimmerViewContainer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rv = (RecyclerView) findViewById(R.id.main_recycler);
+   //     progressBar = (ProgressBar) findViewById(R.id.main_progress);
+        mShimmerViewContainer = findViewById(R.id.shimmer_view_container);
+
+        adapter = new PaginationAdapter(this);
+        linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        rv.setLayoutManager(linearLayoutManager);
+
+        rv.setItemAnimator(new DefaultItemAnimator());
+
+        rv.setAdapter(adapter);
+
+        rv.addOnScrollListener(new PaginationScrollListener(linearLayoutManager) {
+            @Override
+            protected void loadMoreItems() {
+                isLoading = true;
+                currentPage += 1;
+
+                // mocking network delay for API call
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        loadNextPage();
+                    }
+                }, 1000);
+            }
+
+            @Override
+            public int getTotalPageCount() {
+                return TOTAL_PAGES;
+            }
+
+            @Override
+            public boolean isLastPage() {
+                return isLastPage;
+            }
+
+            @Override
+            public boolean isLoading() {
+                return isLoading;
+            }
+        });
+
+        //init service and load data
+        movieService = MovieApi.getClient().create(MovieService.class);
+
+        loadFirstPage();
+        
+    }
+    private void loadFirstPage() {
+        Log.d("working", "loadFirstPage: ");
+
+        callTopRatedMoviesApi().enqueue(new Callback<TopRatedMovies>() {
+            @Override
+            public void onResponse(Call<TopRatedMovies> call, Response<TopRatedMovies> response) {
+                // Got data. Send it to adapter
+
+                List<Result> results = fetchResults(response);
+                Log.e("results", String.valueOf(results.size()));
+              //  progressBar.setVisibility(View.GONE);
+                adapter.addAll(results);
+                mShimmerViewContainer.stopShimmerAnimation();
+                mShimmerViewContainer.setVisibility(View.GONE);
+
+                if (currentPage <= TOTAL_PAGES) adapter.addLoadingFooter();
+                else isLastPage = true;
+            }
+
+            @Override
+            public void onFailure(Call<TopRatedMovies> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+
+    }
+    private List<Result> fetchResults(Response<TopRatedMovies> response) {
+        TopRatedMovies topRatedMovies = response.body();
+        return topRatedMovies.getResults();
+    }
+
+    private void loadNextPage() {
+        Log.d("working", "loadNextPage: " + currentPage);
+
+        callTopRatedMoviesApi().enqueue(new Callback<TopRatedMovies>() {
+            @Override
+            public void onResponse(Call<TopRatedMovies> call, Response<TopRatedMovies> response) {
+                adapter.removeLoadingFooter();
+                isLoading = false;
+
+                List<Result> results = fetchResults(response);
+                Log.e("results", String.valueOf(results.size()));
+                adapter.addAll(results);
+
+                if (currentPage != TOTAL_PAGES) adapter.addLoadingFooter();
+                else isLastPage = true;
+            }
+
+            @Override
+            public void onFailure(Call<TopRatedMovies> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+    }
+    private Call<TopRatedMovies> callTopRatedMoviesApi() {
+        return movieService.getTopRatedMovies(
+                getString(R.string.my_api_key),
+                "en_US",
+                currentPage
+        );
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mShimmerViewContainer.startShimmerAnimation();
+    }
+
+    @Override
+    public void onPause() {
+        mShimmerViewContainer.stopShimmerAnimation();
+        super.onPause();
+    }
+}
---
+++ app/src/main/java/com/dbug/paginationwithretrofit/api/MovieService.java
@@ -0,0 +1,27 @@
+package com.dbug.paginationwithretrofit.api;
+
+
+
+
+import com.dbug.paginationwithretrofit.models.TopRatedMovies;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Pagination
+ * Created by Suleiman19 on 10/27/16.
+ * Copyright (c) 2016. Suleiman Ali Shakir. All rights reserved.
+ */
+
+public interface MovieService {
+
+    @GET("movie/top_rated")
+    Call<TopRatedMovies> getTopRatedMovies(
+            @Query("api_key") String apiKey,
+            @Query("language") String language,
+            @Query("page") int pageIndex
+    );
+
+}
